8302f98db20021c78da8da29e8df9d8af0b0f535
Dan Auerbach <dauerbach@ucsd.edu>
2016-09-12 13:18:02 -0700

    add outer loop for segment processing

--- ida/calibration/absolute.py
+++ ida/calibration/absolute.py
@@ -27,9 +27,11 @@ import yaml
 #import collections
 
 from fabulous.color import red, bold
-from obspy import read, UTCDateTime #, Stream, Trace
+from obspy import read, UTCDateTime, Stream#, Trace
+from obspy.signal.invsim import evalresp
 
 from ida.utils import i10get, pimseed
+from ida.signals.utils import time_offset
 from ida.calibration.shaketable import rename_chan
 
 
@@ -40,18 +42,12 @@ class AbsOnsiteConfig(object):
         self.errs = []
 
         # in-memory streams of source miniseed data
-        self._ref_azi_strm = None
-        self._ref_abs_strm = None
-        self._pri_azi_strm = None
-        self._pri_abs_strm = None
-        self._sec_azi_strm = None
-        self._sec_abs_strm = None
-        self._ref_azi_strm_proc = None
-        self._ref_abs_strm_proc = None
-        self._pri_azi_strm_proc = None
-        self._pri_abs_strm_proc = None
-        self._sec_azi_strm_proc = None
-        self._sec_abs_strm_proc = None
+        self._ref_azi_strm = Stream()
+        self._ref_abs_strm = Stream()
+        self._pri_azi_strm = Stream()
+        self._pri_abs_strm = Stream()
+        self._sec_azi_strm = Stream()
+        self._sec_abs_strm = Stream()
 
         # in-memory streams of miniseed data prepped for analysis
 
@@ -164,12 +160,68 @@ class AbsOnsiteConfig(object):
         return self._config['absolute_ref_data']['endtime_iso']
 
     @property
-    def segment_size_samples(self):
-        return self._config['segment_size_samples']
+    def segment_size_secs(self):
+        return self._config['segment_size_secs']
 
     @property
     def minimum_segment_cnt(self):
         return self._config['minimum_segment_cnt']
+
+    @property
+    def correlation_segment_size(self):
+        return self._config['correlation_segment_size_secs']
+
+    def correct_ref_abs_time(self):
+
+        offset = 0
+        cval = 0.0
+        cfunc = []
+        emsg = ''
+
+        if self._ref_abs_strm.select(channel='BHZ'):
+           ref_z = self._ref_abs_strm.select(channel='BHZ').copy()
+        else:
+           emsg = 'Error: Can not correlate reference and site sensor time series. ' + \
+                  'No vertical channel found in reference sensor data'
+           return offset, cval, cfunc, emsg
+
+        if self._sec_abs_strm.select(channel='BHZ'):
+           sensor_z = self._sec_abs_strm.select(channel='BHZ').copy()
+        elif self._pri_abs_strm.select(channel='BHZ'):
+           sensor_z = self._pri_abs_strm.select(channel='BHZ').copy()
+        else:
+           emsg = 'Error: Can not correlate reference and site sensor time series. ' + \
+                          'No vertical channel found in primary or secondary sensor data'
+           return offset, cval, cfunc, emsg
+
+
+       # may need to decimate ref data if using 20Hz Primary
+        ref_z.decimate(round(ref_z[0].stats.sampling_rate / sensor_z[0].stats.sampling_rate))
+
+        if sensor_z and ref_z:
+           self.ref_z = ref_z
+           self.sensor_z = sensor_z
+           ref_z.filter('bandpass',
+                        freqmin=self._config['analysis_bandpass'][0],
+                        freqmax=self._config['analysis_bandpass'][1])
+           ref_z.normalize()
+           sensor_z.filter('bandpass',
+                        freqmin=self._config['analysis_bandpass'][0],
+                        freqmax=self._config['analysis_bandpass'][1])
+           sensor_z.normalize()
+           # take middle two hours for time series correlation
+           dur = sensor_z[0].stats.endtime - sensor_z[0].stats.starttime
+           start_t = sensor_z[0].stats.starttime + dur/2 - self.correlation_segment_size/2
+           #start_t = sensor_z[0].stats.starttime + 60*60*4
+           end_t = start_t + self.correlation_segment_size
+           ref_z.trim(start_t, end_t)
+           sensor_z.trim(start_t, end_t)
+
+           offset, cval, cfunc, emsg = time_offset(sensor_z[0], ref_z[0])
+           for tr in self._ref_abs_strm:
+               tr.stats.starttime = tr.stats.starttime + offset
+
+        return offset, cval, cfunc, emsg
 
     def read_ref_data(self):
         """
@@ -180,12 +232,19 @@ class AbsOnsiteConfig(object):
                 self._ref_azi_strm = read(self.ref_azi_seed_file, format='MSEED')
             except:
                 print(red(bold('Error reading reference azimuth data: ' + self.ref_azi_seed_file)))
+            else:
+                self._ref_azi_strm.trim(starttime=self.azi_starttime, endtime=self.azi_endtime)
+                self._ref_azi_strm.merge()
+
 
         if self._config['process_absolute'] and self.ref_abs_seed_file:
             try:
                 self._ref_abs_strm = read(self.ref_abs_seed_file, format='MSEED')
             except:
                 print(red(bold('Error reading reference absolute data: ' + self.ref_abs_seed_file)))
+            else:
+                self._ref_abs_strm.trim(starttime=self.abs_starttime, endtime=self.abs_endtime)
+                self._ref_abs_strm.merge()
 
         self.clean_ref_channels()
 
@@ -207,20 +266,7 @@ class AbsOnsiteConfig(object):
                 tr.stats.station = self._config['field_kit_metadata']['station']
                 tr.stats.location = self._config['field_kit_metadata']['location']
 
-    def trim_data(self):
-        """
-        Trim all existing streams to azi/abs start/end times
-        """
-
-        if self._ref_azi_strm: self._ref_azi_strm.trim(self.azi_starttime, self.azi_endtime)
-        if self._pri_azi_strm: self._pri_azi_strm.trim(self.azi_starttime, self.azi_endtime)
-        if self._sec_azi_strm: self._sec_azi_strm.trim(self.azi_starttime, self.azi_endtime)
-        if self._ref_abs_strm: self._ref_abs_strm.trim(self.abs_starttime, self.abs_endtime)
-        if self._pri_abs_strm: self._pri_abs_strm.trim(self.abs_starttime, self.abs_endtime)
-        if self._sec_abs_strm: self._sec_abs_strm.trim(self.abs_starttime, self.abs_endtime)
-
-
-    def get_sensor_data(self):
+    def read_sensor_data(self):
         """
         Retrieve IDA10 data from IDA Archive and comvert to miniseed for azi/abs time periods
         """
@@ -239,6 +285,8 @@ class AbsOnsiteConfig(object):
                    outfn=outname+'.i10')
             pimseed(self._config['site_settings']['station'], outname+'.i10', outname+'.ms')
             self._pri_azi_strm = read(outname + '.ms')
+            self._pri_azi_strm.trim(starttime=self.azi_starttime, endtime=self.azi_endtime)
+            self._pri_azi_strm.merge()
 
             print('Retrieving secondary sensor data for azimuth period...')
             outname = './{}_{}_{}'.format(self._config['site_settings']['station'],
@@ -252,6 +300,8 @@ class AbsOnsiteConfig(object):
                    outfn=outname + '.i10')
             pimseed(self._config['site_settings']['station'], outname + '.i10', outname + '.ms')
             self._sec_azi_strm = read(outname + '.ms')
+            self._sec_azi_strm.trim(starttime=self.azi_starttime, endtime=self.azi_endtime)
+            self._sec_azi_strm.merge()
 
         if self._config['process_absolute'] and self._ref_abs_strm:
             print('Retrieving primary sensor data for absolute period...')
@@ -266,6 +316,8 @@ class AbsOnsiteConfig(object):
                    outfn=outname+'.i10')
             pimseed(self._config['site_settings']['station'], outname+'.i10', outname+'.ms')
             self._pri_abs_strm = read(outname + '.ms')
+            self._pri_abs_strm.trim(starttime=self.abs_starttime, endtime=self.abs_endtime)
+            self._pri_abs_strm.merge()
 
             print('Retrieving secondary sensor data for absolute period...')
             outname = './{}_{}_{}'.format(self._config['site_settings']['station'],
@@ -279,20 +331,25 @@ class AbsOnsiteConfig(object):
                    outfn=outname+'.i10')
             pimseed(self._config['site_settings']['station'], outname+'.i10', outname+'.ms')
             self._sec_abs_strm = read(outname + '.ms')
+            self._sec_abs_strm.trim(starttime=self.abs_starttime, endtime=self.abs_endtime)
+            self._sec_abs_strm.merge()
 
 
     def _pri_chanloc_codes(self):
-        if self._ref_azi_strm:
-            return [tr.stats.channel+self._config['site_settings']['pri_sensor_loc'] for tr in self._pri_azi_strm]
+        chans = self._config['site_settings']['pri_sensor_chans'].split(',')
+        loc = self._config['site_settings']['pri_sensor_loc']
+        if chans :
+            return ','.join([chan+loc for chan in chans])
         else:
             return ''
 
     def _sec_chanloc_codes(self):
-        if self._ref_azi_strm:
-            return [tr.stats.channel+self._config['site_settings']['sec_sensor_loc'] for tr in self._sec_azi_strm]
+        chans = self._config['site_settings']['sec_sensor_chans'].split(',')
+        loc = self._config['site_settings']['sec_sensor_loc']
+        if chans :
+            return ','.join([chan+loc for chan in chans])
         else:
             return ''
-
 
     def compare_streams(self, ref_st, sen_st):
         """
@@ -308,7 +365,88 @@ class AbsOnsiteConfig(object):
 
 
         """
-        pass
+        ref_z_tr = ref_st.select(component='Z').copy()
+        ref_1_tr = ref_st.select(component='1').copy()
+        ref_2_tr = ref_st.select(component='2').copy()
+        sen_z_tr = sen_st.select(component='Z').copy()
+        sen_1_tr = sen_st.select(component='1').copy()
+        sen_2_tr = sen_st.select(component='2').copy()
+        # construct RESP file filename for sensor_trace
+
+############# NEED TO PUT RESP FILE LOCATIONS (REF, PRI, SEC) IN YAML
+############# EMPTY STRING ==> USE $SEEDRESP
+
+        resp_file = 'RESP.{}.{}.{}.{}'.format(
+            sen_z_tr.stats.network, sen_z_tr.stats.station, sen_z_tr.stats.loc, sen_z_tr.stats.channel
+        )
+        resp_filepath = os.path.join(self._config['resp_file_location'], resp_file)
+        sen_z_resp = evalresp(1/sen_z_tr.stats.sampling_rate,
+                            sen_z_tr.stats.npts,
+                            resp_filepath,
+                            sen_z_tr.stats.starttime,
+                            station=sen_z_tr.stats.station,
+                            channel=sen_z_tr.stats.channel,
+                            network=sen_z_tr.stats.network,
+                            locid=sen_z_tr.stats.loc,
+                            units='DIS')
+
+        resp_file = 'RESP.{}.{}.{}.{}'.format(
+            sen_1_tr.stats.network, sen_1_tr.stats.station, sen_1_tr.stats.loc, sen_1_tr.stats.channel
+        )
+        resp_filepath = os.path.join(self._config['resp_file_location'], resp_file)
+        sen_1_resp = evalresp(1/sen_1_tr.stats.sampling_rate,
+                            sen_1_tr.stats.npts,
+                            resp_filepath,
+                            sen_1_tr.stats.starttime,
+                            station=sen_1_tr.stats.station,
+                            channel=sen_1_tr.stats.channel,
+                            network=sen_1_tr.stats.network,
+                            locid=sen_1_tr.stats.loc,
+                            units='DIS')
+
+        resp_file = 'RESP.{}.{}.{}.{}'.format(
+            sen_2_tr.stats.network, sen_2_tr.stats.station, sen_2_tr.stats.loc, sen_2_tr.stats.channel
+        )
+        resp_filepath = os.path.join(self._config['resp_file_location'], resp_file)
+        sen_2_resp = evalresp(1/sen_2_tr.stats.sampling_rate,
+                            sen_2_tr.stats.npts,
+                            resp_filepath,
+                            sen_2_tr.stats.starttime,
+                            station=sen_2_tr.stats.station,
+                            channel=sen_2_tr.stats.channel,
+                            network=sen_2_tr.stats.network,
+                            locid=sen_2_tr.stats.loc,
+                            units='DIS')
+
+        start_t = ref_z_tr.stats.starttime
+        while start_t + self.segment_size_secs < ref_z_tr.stats.endtime:
+
+            ref_z_seg = ref_z_tr.slice(starttime=start_t, endtime=start_t + self.segment_size_secs)
+            ref_1_seg = ref_1_tr.slice(starttime=start_t, endtime=start_t + self.segment_size_secs)
+            ref_2_seg = ref_2_tr.slice(starttime=start_t, endtime=start_t + self.segment_size_secs)
+            sen_z_seg = sen_z_tr.slice(starttime=start_t, endtime=start_t + self.segment_size_secs)
+            sen_1_seg = sen_1_tr.slice(starttime=start_t, endtime=start_t + self.segment_size_secs)
+            sen_2_seg = sen_2_tr.slice(starttime=start_t, endtime=start_t + self.segment_size_secs)
+
+            # deconvolve sensor resp from sen data
+    # DEConvolving ref data with nominal response...
+    refdata    = ref_trace.data.astype(float32)
+    mean       = refdata.mean()
+    refdata   -= mean
+    refdata_fft    = rfft(refdata)
+    refdata_fft   *= fresp
+    ref_wth_resp  = irfft(refdata_fft, npts)
+    ref_wth_resp *= (shake_m_per_volt/digi_sens_cnts_per_volt)
+            # convolve sen data with ref resp
+    # Convolving ref data with nominal response...
+    refdata    = ref_trace.data.astype(float32)
+    mean       = refdata.mean()
+    refdata   -= mean
+    refdata_fft    = rfft(refdata)
+    refdata_fft   *= fresp
+    ref_wth_resp  = irfft(refdata_fft, npts)
+    ref_wth_resp *= (shake_m_per_volt/digi_sens_cnts_per_volt)
+            # trim to 768 secs
 
     def compare_traces(self, ref_tr, sen_tr, segment_size_samples=1024*40):
         """These """
