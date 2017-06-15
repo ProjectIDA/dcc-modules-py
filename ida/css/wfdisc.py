eace8bc5aa81c47167df8b64f1dfab477e3435ff
Dan Auerbach <dauerbach@ucsd.edu>
2017-06-15 16:01:49 -0700

    Add *args **kwargs to and add logic for skip_bindata

--- ida/css/wfdisc.py
+++ ida/css/wfdisc.py
@@ -5,27 +5,27 @@ import numpy as np
 from obspy import Trace, Stream, UTCDateTime
 from obspy.core.trace import Stats
 from .exceptions import (
-    WfdiscFileNotFoundException, 
-    WfdiscSegmentRecordInvalidSize, WfdiscSegmentTimeFormatError, WfdiscSegmentWfidFormatError, 
-    WfdiscSegmentChanidFormatError, WfdiscSegmentJdateFormatError, WfdiscSegmentEndtimeFormatError, 
-    WfdiscSegmentNsampFormatError, WfdiscSegmentSamprateFormatError, WfdiscSegmentCalibFormatError, 
-    WfdiscSegmentCalperFormatError, WfdiscSegmentFileNotFoundError, WfdiscSegmentDatatypeValueError, 
+    WfdiscFileNotFoundException,
+    WfdiscSegmentRecordInvalidSize, WfdiscSegmentTimeFormatError, WfdiscSegmentWfidFormatError,
+    WfdiscSegmentChanidFormatError, WfdiscSegmentJdateFormatError, WfdiscSegmentEndtimeFormatError,
+    WfdiscSegmentNsampFormatError, WfdiscSegmentSamprateFormatError, WfdiscSegmentCalibFormatError,
+    WfdiscSegmentCalperFormatError, WfdiscSegmentFileNotFoundError, WfdiscSegmentDatatypeValueError,
     WfdiscSegmentFoffFormatError, WfdiscSegmentCommidFormatError, WfdiscSegmentLddateFormatError
 )
 
 class WfdiscFile(object):
-    
-    def __init__(self, filename=None):
-        
-        if filename and not os.path.exists(filename):
-            raise WfdiscFileNotFoundException(filename)
-        
-        self._fpath = os.path.split(filename)[0] 
+
+    def __init__(self, filename, *args, **kwargs):
+
+        if not os.path.exists(filename):
+            raise WfdiscFileNotFoundException(filename or '<no filename supplied>')
+
+        self._fpath = os.path.split(filename)[0]
         self._segments = []
         # self._samples = []
         with open(filename, 'rt') as wffil:
             for segrec in wffil:
-                newwfseg = WfdiscSegment(filename, segrec)
+                newwfseg = WfdiscSegment(filename, segrec, *args, **kwargs)
                 ok, err = newwfseg.load_wf_data()
                 if ok:
                     self._segments.append(newwfseg)
@@ -36,11 +36,17 @@ class WfdiscFile(object):
     @property
     def segment_cnt(self):
         return len(self._segments)
-    
+
     @property
     def segments(self):
         return self._segments
-    
+
+    def __len__(self):
+        return len(self._segments)
+
+    def __getitem__(self, item):
+        return self._segments[item]
+
     def write_miniseed(self, filename: str, network: str) -> (bool, str):
 
         st = Stream()
@@ -60,7 +66,7 @@ class WfdiscFile(object):
             header['npts'] = seg.samples.size
             header['starttime'] = UTCDateTime(seg.seginfo[seg.KEY_TIME])
             header['dataquality'] = 'R'
-            header['mseed'] = {}            
+            header['mseed'] = {}
             if sys.byteorder == 'big':
                 header['mseed']['byteorder'] = '>'
             elif sys.byteorder == 'little':
@@ -81,7 +87,7 @@ class WfdiscFile(object):
 
 class WfdiscSegment(object):
     """Object representing the data and metadata of a single record in a Wfdisc file"""
-    
+
     SEGMENT_REC_FIELDCNT = 20
     KEY_STA = 'sta'
     KEY_CHAN = 'chan'
@@ -103,22 +109,24 @@ class WfdiscSegment(object):
     KEY_FOFF = 'foff'
     KEY_COMMID = 'commid'
     KEY_LDDATE = 'lddate'
-    
+
     KEYS_DATATYPES = {
         's3': {'size': 3}
     }
-        
-    def __init__(self, wfdisc_fn: str, segrec: str):
+
+    def __init__(self, wfdisc_fn: str, segrec: str, *args, **kwargs):
         """Constructor for WfdiscSegment object
 
         Args:
-            wfdisc_fn: Source wfdisc file path. Needed to obtain absolute location of WF data file 
+            wfdisc_fn: Source wfdisc file path. Needed to obtain absolute location of WF data file
             segrec: Record from Wfdisc fiel with which to create this segment
         """
-        
+
+        self._skip_bindata = kwargs.get('skip_bindata', False)
+
         self.seginfo = {}
         self._samples = None
-        self._fpath = wfdisc_fn 
+        self._fpath = wfdisc_fn
 
         flds = segrec.split()
         if len(flds) == self.SEGMENT_REC_FIELDCNT:
@@ -160,15 +168,15 @@ class WfdiscSegment(object):
                 self.seginfo[self.KEY_CALPER] = float(flds[10])
             except ValueError as e:
                 raise WfdiscSegmentCalperFormatError("Error converting CALPER to float: {}".format(flds[10]))
-                
+
             self.seginfo[self.KEY_INSTYPE] = flds[11]
             self.seginfo[self.KEY_SEGTYPE] = flds[12]
-            
-            if flds[13] in self.KEYS_DATATYPES:
+
+            if self._skip_bindata or (flds[13] in self.KEYS_DATATYPES):
                 self.seginfo[self.KEY_DATATYPE] = flds[13]
             else:
                 raise WfdiscSegmentDatatypeValueError('Invalid data type: {}'.format(flds[13]))
-            
+
             self.seginfo[self.KEY_CLIP] = flds[14]
             self.seginfo[self.KEY_DIR] = flds[15]
             self.seginfo[self.KEY_DFILE] = flds[16]
@@ -185,48 +193,49 @@ class WfdiscSegment(object):
                 self.seginfo[self.KEY_LDDATE] = float(flds[19])
             except ValueError as e:
                 raise WfdiscSegmentLddateFormatError("Error converting LDDATE to float: {}".format(flds[19]))
-            
+
         else:
             raise WfdiscSegmentRecordInvalidSize(len(flds))
-               
-        
+
+
     @property
     def samples(self):
         return self._samples
-    
+
     @samples.setter
     def samples(self, value):
         self._samples = value
 
     def load_wf_data(self) -> (bool, str):
         """Read WF datam from WF binary file into self.samples
-        
+
         Returns:
             Success: flag to indicate load success (True) or failure (False)
             Errmsg: to indicate nature of error. '' if successful.
         """
 
-        start_byte = self.seginfo[self.KEY_FOFF]
-        sample_size = self.KEYS_DATATYPES[self.seginfo[self.KEY_DATATYPE]]['size']
-        bytes_to_read = sample_size * self.seginfo[self.KEY_NSAMP]
+        if not self._skip_bindata:
+            start_byte = self.seginfo[self.KEY_FOFF]
+            sample_size = self.KEYS_DATATYPES[self.seginfo[self.KEY_DATATYPE]]['size']
+            bytes_to_read = sample_size * self.seginfo[self.KEY_NSAMP]
 
-        wffn = os.path.join(os.path.split(self._fpath)[0], self.seginfo[self.KEY_DIR], self.seginfo[self.KEY_DFILE])
-        if os.path.exists(wffn) and os.path.isfile(wffn):
-            with open(wffn, 'rb') as wffl:
-                wffl.seek(start_byte)
-                rawdata = wffl.read(bytes_to_read)
-        else:
-            return False, '{}: File not found: {}'.format(currentframe().f_code.co_name, wffn)
+            wffn = os.path.join(os.path.split(self._fpath)[0], self.seginfo[self.KEY_DIR], self.seginfo[self.KEY_DFILE])
+            if os.path.exists(wffn) and os.path.isfile(wffn):
+                with open(wffn, 'rb') as wffl:
+                    wffl.seek(start_byte)
+                    rawdata = wffl.read(bytes_to_read)
+            else:
+                return False, '{}: File not found: {}'.format(currentframe().f_code.co_name, wffn)
 
-        # convert data based on datatype
-        if self.seginfo[self.KEY_DATATYPE] == 's3':
-            wfdata = WfdiscSegment.convert_s3(rawdata)
-        else:
-            return False, '{}: Unsupported wfdisc datatype: {}'.format(currentframe().f_code.co_name, 
-                                                                       self.seginfo[self.KEY_DATATYPE])
+            # convert data based on datatype
+            if self.seginfo[self.KEY_DATATYPE] == 's3':
+                wfdata = WfdiscSegment.convert_s3(rawdata)
+            else:
+                return False, '{}: Unsupported wfdisc datatype: {}'.format(currentframe().f_code.co_name,
+                                                                           self.seginfo[self.KEY_DATATYPE])
 
-        self.samples = wfdata
-        
+            self.samples = wfdata
+
         return True, ''
 
     @classmethod
@@ -245,7 +254,7 @@ class WfdiscSegment(object):
         for ndx in range(0, len(rawdata), 3):
             val = (rawdata[ndx] << 16) + (rawdata[ndx + 1] << 8) + (rawdata[ndx + 2])
             if val > pow(2, 23):
-                val -= pow(2, 24) 
+                val -= pow(2, 24)
             wfdata.append(val)
 
         return np.array(wfdata, np.int32)
