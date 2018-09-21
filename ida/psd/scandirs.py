9488956f179219b67995191bfbd51f7c4680bdf6
D. Scott Bargabus <dbargabus@ucsd.edu>
2018-09-21 16:47:37 -0700

    Revert to code that was "lost" in the git merge...

--- ida/psd/scandirs.py
+++ ida/psd/scandirs.py
@@ -4,41 +4,90 @@ import argparse
 import os
 import glob
 import json
+import pprint
+from calendar import month_abbr
 
-def process_dir(idroot):
-    # must start in root of tree you're processing
-    entries = []
-    separator = '/'
+def process_dir(rootdir):
+    """ must start in root of tree you're processing. it should be the directory containing YYYY subdirs
+        Constructs dir datatree that can be used by jinga templates to construct the nav menu
 
-    for entry in sorted(glob.glob('*')):
-        if entry in ['.', '..']:
-            continue
+        Does not use recursion due to somewhat specific logic needed at year/month and day levels.
+    """
+    savedir = os.getcwd()
+    os.chdir(rootdir)
 
-        if os.path.isdir(entry):
-            items = [idroot, entry]
-            nextroot = separator.join( str(x) for x in items)
+    datetree = []
 
-            # the keys to this dictionary are required by the treeview JS module
-            new_entry = {}
-            new_entry["id"] = nextroot.lstrip(separator) # trim leading '/'
-            dateList = new_entry["id"].split(separator)
-            dateStr = ""
-            # need to do this more pythonically
-            if len(dateList) is 1:
-                dateStr = dateList[0]
-            if len(dateList) is 2:
-                dateStr = dateList[1] + separator + dateList[0]
-            if len(dateList) is 3:
-                dateStr = dateList[1] + separator + dateList[2] + separator + dateList[0]
-            
-            new_entry["text"] = dateStr
+    for ayear in sorted(glob.glob('????')):
 
-            os.chdir(entry)
-            new_entry['children'] = process_dir(nextroot)
+        if os.path.isdir(ayear):
+
+            yearinfo = { 'year': ayear,
+                         'months': []
+                       }
+            os.chdir(ayear)
+
+            for amonth in sorted(glob.glob('??')):
+
+                monthinfo = { 'month': amonth,
+                              'monthname': month_abbr[int(amonth)],
+                              'days': []
+                            }
+
+                os.chdir(amonth)
+                for aday in sorted(glob.glob('??')):
+                    monthinfo['days'].append(aday)
+
+                yearinfo['months'].append(monthinfo)
+                os.chdir("..")
+
+            datetree.append(yearinfo)
             os.chdir("..")
-            
-            entries.append(new_entry)
-    
-    return entries
 
-################################################################################
+    os.chdir(savedir)
+
+    return datetree
+
+
+
+#def process_dir(idroot):
+#    # must start in root of tree you're processing
+#    entries = []
+#    separator = '/'
+#
+#    for entry in sorted(glob.glob('*')):
+#        if entry in ['.', '..']:
+#            continue
+#
+#        if os.path.isdir(entry):
+#            items = [idroot, entry]
+#            nextroot = separator.join( str(x) for x in items)
+#
+#            new_entry = {}
+#            new_entry["id"] = nextroot.lstrip(separator) # trim leading '/'
+#            dateList = new_entry["id"].split(separator)
+#            dateStr = ""
+#            # need to do this more pythonically
+#            if len(dateList) is 1:
+#                dateStr = dateList[0]
+#            if len(dateList) is 2:
+#                dateStr = dateList[1] + separator + dateList[0]
+#            if len(dateList) is 3:
+#                dateStr = dateList[1] + separator + dateList[2] + separator + dateList[0]
+#            
+#            new_entry["text"] = dateStr
+#
+#            os.chdir(entry)
+#            new_entry['children'] = process_dir(nextroot)
+#            os.chdir("..")
+#            
+#            entries.append(new_entry)
+#
+#    pp = pprint.PrettyPrinter(indent=4)
+#    print("\n\nENTRIES: \n\n")
+#    pp.pprint(entries)
+#    print("\n\n")
+#
+#    return entries
+#
+#################################################################################
